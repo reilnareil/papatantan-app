af2093f6e7f41a73e3c2771a432cf74665e8d63c
Reilan Cadubla <reilancadubla@gmail.com>
2026-06-14 19:20:18 +0800

    for init

---
+++ src/app/reservation/reservation.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
+import { ReservationListComponent } from '../reservation-list/reservation-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from "../app-routing.module";
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    ReservationFormComponent,
+    ReservationListComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    RouterModule
+]
+})
+export class ReservationModule { }
